b82e849e22cb1a96b198b916049a216f289eb4e1
niuzheng131400 <771036148@qq.com>
2022-10-26 09:23:33 +0800

    提交

---
+++ vendor/composer/installed.php
@@ -0,0 +1,45 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'niuz',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1349edfaa2ef37cacb86917fe088963f9c1c46e5',
+        'name' => 'niuz/slideverifycodestatic',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(
+                0 => '2.x-dev',
+            ),
+            'reference' => '2a9170263fcd9cc4fd0b50917293c21d6c1a5bfe',
+            'dev_requirement' => false,
+        ),
+        'niuz/slideverifycodestatic' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'niuz',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1349edfaa2ef37cacb86917fe088963f9c1c46e5',
+            'dev_requirement' => false,
+        ),
+        'oomphinc/composer-installers-extender' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../oomphinc/composer-installers-extender',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'cbf4b6f9a24153b785d09eee755b995ba87bd5f9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
